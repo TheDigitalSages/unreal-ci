6e5773e5391ce0e9b1719931fed9c96fe65fcc41
Edward Beazer <eddiebeazer@gmail.com>
2023-12-05 12:26:47 -0500

    chore: Added more coverage to clean test (#12)

--- pkgs/clean/clean_test.go
+++ pkgs/clean/clean_test.go
@@ -11,9 +11,15 @@ func TestCleanDirectory(t *testing.T) {
 	err := os.Mkdir(dirToTest, 777)
 	assert.Nil(t, err)
 
+	// Testing that we can successfully delete the "Test" directory
 	err = Dist(dirToTest)
 	assert.Nil(t, err)
 
+	// Testing that we exit if the directory isn't provided
 	err = Dist("")
 	assert.NotNil(t, err)
+
+	// Testing fake directory that does not exists
+	err = Dist("aeed/asd//")
+	assert.Nil(t, err)
 }
